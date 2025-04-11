9f8abf8c99eb21db3c5c9c65c73da242cc34326b
NTA_LamLDM <lamldm.nta@gmail.com>
2025-04-11 10:23:07 +0700

    create component

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+function Header() {
+  return <h1>Header</h1>
+}
+
+function Footer() {
+  return <div>Footer</div>
+}
+
+function Pizza() {
+  return (
+    <div>
+      <img src="https://placehold.co/160x90" alt="Pizza" />
+      <h2>Pizza Title</h2>
+      <div>Pizza Description</div>
+    </div>
+  )
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our Menu</h2>
+      <Pizza />
+      <Pizza />
+    </div>
+  )
+}
+
 export function App() {
-  return <h1>Hello React!!!!</h1>
+  return (
+    <div>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
