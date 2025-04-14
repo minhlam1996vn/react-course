22e64fb2daba91be767905f561a8ed6b55229ec6
NTA_LamLDM <lamldm.nta@gmail.com>
2025-04-14 10:35:28 +0700

    rendering list data

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -2,41 +2,86 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
+const pizzaData = [
+  {
+    name: 'Focaccia',
+    ingredients: 'Bread with italian olive oil and rosemary',
+    price: 6,
+    photoName: 'http://placehold.co/160x90',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Margherita',
+    ingredients: 'Tomato and mozarella',
+    price: 10,
+    photoName: 'http://placehold.co/160x90',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Spinaci',
+    ingredients: 'Tomato, mozarella, spinach, and ricotta cheese',
+    price: 12,
+    photoName: 'http://placehold.co/160x90',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Funghi',
+    ingredients: 'Tomato, mozarella, mushrooms, and onion',
+    price: 12,
+    photoName: 'http://placehold.co/160x90',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Salamino',
+    ingredients: 'Tomato, mozarella, and pepperoni',
+    price: 15,
+    photoName: 'http://placehold.co/160x90',
+    soldOut: true,
+  },
+  {
+    name: 'Pizza Prosciutto',
+    ingredients: 'Tomato, mozarella, ham, aragula, and burrata cheese',
+    price: 18,
+    photoName: 'http://placehold.co/160x90',
+    soldOut: false,
+  },
+]
+
 function Header() {
-  return <h1>Header</h1>
+  //const style = { fontSize: '24px', color: 'red', textTransform: 'uppercase' };
+  return (
+    <div className="header">
+      <h1>Leela Pizza App</h1>
+    </div>
+  )
 }
 
 function Footer() {
-  return <div>Footer</div>
+  return <div>Open Today</div>
 }
 
 function Pizza(props) {
   return (
-    <div>
-      <img src={props.photoName} alt={props.name} />
-      <h3>{props.name}</h3>
-      <p>{props.ingredients}</p>
-      <span>{props.price + 4}</span>
-    </div>
+    <li className="pizza">
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+      <div>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>{props.pizzaObj.price}</span>
+      </div>
+    </li>
   )
 }
 
 function Menu() {
   return (
-    <div>
+    <div className="menu">
       <h2>Our Menu</h2>
-      <Pizza
-        name="Pizza Prosciutto"
-        ingredients="Tomato, mozarella"
-        price={18}
-        photoName="http://placehold.co/160x90"
-      />
-      <Pizza
-        name="Pizza Salamino"
-        ingredients="Tomato, mozarella, ham, aragula, and burrata cheese"
-        price={4}
-        photoName="http://placehold.co/160x90"
-      />
+      <ul className="pizzas">
+        {pizzaData.map((pizza) => (
+          <Pizza key={pizza.name} pizzaObj={pizza} />
+        ))}
+      </ul>
     </div>
   )
 }
