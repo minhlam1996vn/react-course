4c3ce96f768f5076fe2a2cb315667cccae23ca79
NTA_LamLDM <lamldm.nta@gmail.com>
2025-04-21 11:47:49 +0700

    grocery project setup

--- grocery-list/src/App.jsx
+++ grocery-list/src/App.jsx
@@ -1,7 +1,18 @@
 import './App.css'
+import AddItem from './components/AddItem'
+import Header from './components/Header'
+import ItemsList from './components/ItemsList'
+import Footer from './components/Footer'
 
 function App() {
-  return <div className="container">APP</div>
+  return (
+    <div className="container">
+      <Header />
+      <AddItem />
+      <ItemsList />
+      <Footer />
+    </div>
+  )
 }
 
 export default App
