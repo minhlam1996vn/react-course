6093d3b94b352b77653ef0067ad0fe3e3654179c
NTA_LamLDM <lamldm.nta@gmail.com>
2025-04-22 16:08:41 +0700

    Send Data from child to parent

--- grocery-list/src/App.jsx
+++ grocery-list/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './App.css'
 import AddItem from './components/AddItem'
 import Header from './components/Header'
@@ -5,12 +6,18 @@ import ItemsList from './components/ItemsList'
 import Footer from './components/Footer'
 
 function App() {
+  const [items, setItems] = useState([])
+
+  function addItem(item) {
+    setItems((items) => [...items, item])
+  }
+
   return (
     <div>
       <Header />
       <div className="container">
-        <AddItem />
-        <ItemsList />
+        <AddItem addItem={addItem} />
+        <ItemsList items={items} />
       </div>
       <Footer />
     </div>
