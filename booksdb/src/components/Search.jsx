10561a7d8ef1873b6d52addbbd6c441995b1005a
NTA_LamLDM <lamldm.nta@gmail.com>
2025-06-11 09:25:43 +0700

    re useref hook

--- booksdb/src/components/Search.jsx
+++ booksdb/src/components/Search.jsx
@@ -1,9 +1,35 @@
+import { useEffect, useRef } from 'react'
+
 export default function Search({ query, setQuery }) {
+  const inputRef = useRef(null)
+
+  useEffect(() => {
+    function callback(e) {
+      console.log(e.code)
+      if (e.code === 'Enter' || e.code === 'NumpadEnter') {
+        const activeElement = document.activeElement
+        inputRef.current.focus()
+        if (activeElement !== inputRef.current) {
+          setQuery('')
+        }
+      }
+    }
+
+    document.addEventListener('keydown', callback)
+    inputRef.current.focus()
+
+    return () => {
+      document.removeEventListener('keydown', callback)
+    }
+  }, [])
+
   return (
     <input
+      className="search"
       type="text"
       placeholder="Search Books..."
       value={query}
+      ref={inputRef}
       onInput={(e) => setQuery(e.target.value)}
     />
   )
