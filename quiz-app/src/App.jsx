8f593534414cc7dcd4754d92b9f7282240abe704
NTA_LamLDM <lamldm.nta@gmail.com>
2025-06-16 14:26:15 +0700

    loading question

--- quiz-app/src/App.jsx
+++ quiz-app/src/App.jsx
@@ -1,12 +1,44 @@
+import { useEffect, useReducer } from 'react'
 import './App.css'
 import Header from './components/Header'
 import Main from './components/Main'
+import Loader from './components/Loader'
+
+const initialState = {
+  questions: [],
+  status: 'loading', //loading, ready, error, active, finished
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return { ...state, questions: action.payload, status: 'ready' }
+
+    case 'dataFailed':
+      return { ...state, status: 'error' }
+  }
+}
 
 function App() {
+  const [{ status, questions }, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(() => {
+    fetch(`http://localhost:9001/questions`)
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: 'dataReceived', payload: data }))
+      .catch((err) => {
+        console.error('Error fetching data:', err)
+        dispatch({ type: 'dataFailed' })
+      })
+  }, [])
+
   return (
     <div className="container">
       <Header />
-      <Main />
+      <Main>
+        {status}
+        {status === 'loading' && <Loader />}
+      </Main>
     </div>
   )
 }
---
+++ quiz-app/src/components/Loader.jsx
@@ -0,0 +1,9 @@
+export default function Loader() {
+  return (
+    <div className="loading">
+      <span className="loader">
+        <div>Loading...</div>
+      </span>
+    </div>
+  )
+}
