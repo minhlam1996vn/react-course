ba89693035bcc0ad3f4c63cb3d76401c60964b7b
NTA_LamLDM <lamldm.nta@gmail.com>
2025-06-16 11:44:49 +0700

    usereducer hook

--- counter-reducer/src/App.jsx
+++ counter-reducer/src/App.jsx
@@ -1,10 +1,10 @@
-import Counter from './Counter'
+// import Counter from './Counter'
 import CounterReducer from './CounterReducer'
 
 export default function App() {
   return (
     <div className="container">
-      <Counter />
+      {/* <Counter /> */}
       <CounterReducer />
     </div>
   )
