a7e1a37077069b6182f034bfd8a8e248f6950f31
NTA_LamLDM <lamldm.nta@gmail.com>
2025-06-19 14:56:51 +0700

    create routers

--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Root from './routes/Root'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+  },
+  {
+    path: 'contact',
+    element: <div>COntact page</div>,
+  },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
