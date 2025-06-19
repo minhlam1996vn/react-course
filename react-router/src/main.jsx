193552d4d254831e835a0bf6539d6cec076f2e22
NTA_LamLDM <lamldm.nta@gmail.com>
2025-06-19 15:09:44 +0700

    Not found page

--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -3,15 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router'
 import Root from './routes/Root'
+import ErrorPage from './routes/ErrorPage'
+import Contact from './routes/Contact'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
   {
-    path: 'contact',
-    element: <div>COntact page</div>,
+    path: 'contacts/1',
+    element: <Contact />,
   },
 ])
 
