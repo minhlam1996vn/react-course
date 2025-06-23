383d2662f7c1d165bd82290a6936c017d7670e4c
NTA_LamLDM <lamldm.nta@gmail.com>
2025-06-23 10:04:22 +0700

    jsx routes

--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -1,7 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from 'react-router'
 import Root, { loader as rootLoader, action as rootAction } from './routes/Root'
 import ErrorPage from './routes/ErrorPage'
 import Contact, {
@@ -12,39 +17,68 @@ import EditContact, { editAction } from './routes/EditContact'
 import { destroyAction } from './routes/Destroy'
 import Index from './routes/Index'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    loader: rootLoader,
-    action: rootAction,
-    children: [
-      {
-        errorElement: <ErrorPage />,
-        children: [
-          { index: true, element: <Index /> },
-          {
-            path: 'contacts/:contactId',
-            element: <Contact />,
-            loader: contactLoader,
-            action: favoriteAction,
-          },
-          {
-            path: 'contacts/:contactId/edit',
-            element: <EditContact />,
-            loader: contactLoader,
-            action: editAction,
-          },
-          {
-            path: 'contacts/:contactId/destroy',
-            action: destroyAction,
-          },
-        ],
-      },
-    ],
-  },
-])
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Root />,
+//     errorElement: <ErrorPage />,
+//     loader: rootLoader,
+//     action: rootAction,
+//     children: [
+//       {
+//         errorElement: <ErrorPage />,
+//         children: [
+//           { index: true, element: <Index /> },
+//           {
+//             path: 'contacts/:contactId',
+//             element: <Contact />,
+//             loader: contactLoader,
+//             action: favoriteAction,
+//           },
+//           {
+//             path: 'contacts/:contactId/edit',
+//             element: <EditContact />,
+//             loader: contactLoader,
+//             action: editAction,
+//           },
+//           {
+//             path: 'contacts/:contactId/destroy',
+//             action: destroyAction,
+//           },
+//         ],
+//       },
+//     ],
+//   },
+// ])
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={<Root />}
+      errorElement={<ErrorPage />}
+      loader={rootLoader}
+      action={rootAction}
+    >
+      <Route errorElement={<ErrorPage />}>
+        <Route index element={<Index />}></Route>
+        <Route
+          path="contacts/:contactId"
+          element={<Contact />}
+          loader={contactLoader}
+          action={favoriteAction}
+        />
+        <Route
+          path="contacts/:contactId/edit"
+          element={<EditContact />}
+          loader={contactLoader}
+          action={editAction}
+        />
+        <Route path="contacts/:contactId/destroy" action={destroyAction} />
+      </Route>
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
