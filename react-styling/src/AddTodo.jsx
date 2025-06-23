5c2ef173092bc975b38a23b5535f1832988c0f8a
NTA_LamLDM <lamldm.nta@gmail.com>
2025-06-23 11:04:42 +0700

    styled-components

--- react-styling/src/AddTodo.jsx
+++ react-styling/src/AddTodo.jsx
@@ -1,20 +1,41 @@
-import { styles } from './styles'
+// import { styles } from './styles'
+import {
+  Container,
+  FormContainer,
+  Header,
+  Label,
+  LabelContainer,
+} from './styles'
 
 export default function AddTodo() {
   return (
-    <div style={styles.Container}>
-      <h2 style={styles.Header}>TODO</h2>
+    // <div style={styles.Container}>
+    //   <h2 style={styles.Header}>TODO</h2>
 
-      <div style={styles.LabelContainer}>
-        <label style={styles.Label}>What needs to be Done?</label>
-      </div>
-      <div style={styles.FormContainer}>
+    //   <div style={styles.LabelContainer}>
+    //     <label style={styles.Label}>What needs to be Done?</label>
+    //   </div>
+    //   <div style={styles.FormContainer}>
+    //     <form>
+    //       <input type="text" />
+    //       <button>Add</button>
+    //     </form>
+    //   </div>
+    // </div>
+
+    <Container>
+      <Header>TODO</Header>
+
+      <LabelContainer>
+        <Label>What needs to be Done?</Label>
+      </LabelContainer>
+      <FormContainer>
         <form>
           <input type="text" />
           <button>Add</button>
         </form>
-      </div>
-    </div>
+      </FormContainer>
+    </Container>
   )
   // return (
   //   <div style={{ display: 'flex', flexDirection: 'column' }}>
