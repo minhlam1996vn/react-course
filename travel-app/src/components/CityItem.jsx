44091f652f558449e247d83e5da70b1d96959f44
NTA_LamLDM <lamldm.nta@gmail.com>
2025-06-24 10:03:06 +0700

    city items

--- travel-app/src/components/CityItem.jsx
+++ travel-app/src/components/CityItem.jsx
@@ -1,3 +1,15 @@
-export default function CityItem() {
-  return <div>CIty Item</div>
+import styles from './CityItem.module.css'
+
+export default function CityItem({ city }) {
+  return (
+    <div className={styles.city}>
+      <div className="d-flex align-items-center">
+        <div>{city.emoji_flag}</div>
+        <div>{city.name}</div>
+      </div>
+      <div>
+        <button className={styles.deleteButton}>&times;</button>
+      </div>
+    </div>
+  )
 }
