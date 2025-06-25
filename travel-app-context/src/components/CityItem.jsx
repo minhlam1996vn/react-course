37725bf651689859856906f768f89e842b52b1b1
NTA_LamLDM <lamldm.nta@gmail.com>
2025-06-25 11:05:44 +0700

    city details context

--- travel-app-context/src/components/CityItem.jsx
+++ travel-app-context/src/components/CityItem.jsx
@@ -1,13 +1,20 @@
 import styles from './CityItem.module.css'
 import { Link } from 'react-router'
+import { useCity } from '../contexts/CityContext'
 
 export default function CityItem({ city }) {
+  const { currentCity } = useCity()
+
   return (
     <Link
       to={`/app/cities/${city.id}?lat=${city.latitude}&long=${city.longitude}`}
       className={styles.cityLink}
     >
-      <div className={styles.city}>
+      <div
+        className={`${styles.city} ${
+          currentCity.id === city.id ? styles.activeCity : ''
+        }`}
+      >
         <div className="d-flex align-items-center">
           <div>{city.emoji_flag}</div>
           <div>{city.name}</div>
