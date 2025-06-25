eebaf0b49387c2a8e377cb6c3019b0093feb73f3
NTA_LamLDM <lamldm.nta@gmail.com>
2025-06-25 08:46:24 +0700

    counter-context

---
+++ counter-context/src/Counter.jsx
@@ -0,0 +1,16 @@
+import CounterButtons from './CounterButtons'
+
+const Counter = ({ count, increment, decrement, reset }) => {
+  return (
+    <div>
+      <div>{count}</div>
+      <CounterButtons
+        increment={increment}
+        decrement={decrement}
+        reset={reset}
+      />
+    </div>
+  )
+}
+
+export default Counter
